73fbd9f95d4ecf129ffe01fa4e1e1958b30dfa27
Hack Reactor Students <accounts@hackreactor.com>
2015-09-16 14:10:10 -0700

    Changed environment variable.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var app = require('./server-config.js');
 
-var port = 4568;
+if (process.env.NODE_ENV === "production") {
+  var port = process.env.PORT;
+} else {
+  var port = 4568;
+}
+
+console.log(port);
 
 app.listen(port);
-
 console.log('Server now listening on port ' + port);
